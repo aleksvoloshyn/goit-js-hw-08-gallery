76ddc0ec88fa0d966f6bd3cf2d1d9b912030c713
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2021-05-26 14:36:31 +0300

    1 - Создание и рендер разметки по массиву данных и предоставленному шаблону.

---
+++ js/script.js
@@ -0,0 +1,35 @@
+import galleryItems from './gallery-items.js';
+
+//ul .gallery
+const galleryListEl = document.querySelector('ul.gallery');
+
+//modal window
+const modalWindowEl = document.getElementsByClassName('lightbox');
+
+//создаем разметку шаблонной строкой
+function createPicturesMarkup(pics) {
+    return pics.map(({ preview}) => {
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+            alt="Tulips"
+          />
+        </a>
+      </li> `
+    }).join('');
+}
+
+//добавляем картинки в список
+function addPictureElementsToList(item) {
+    galleryListEl.insertAdjacentHTML('beforeend', createPicturesMarkup(item));
+}
+
+addPictureElementsToList(galleryItems);
+
+   
+
+
+
