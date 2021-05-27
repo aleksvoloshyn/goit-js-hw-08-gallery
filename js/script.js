0b3bed3da9686b18f4677cda67232445dab59b80
Aleksvoloshyn <alexx.voloshyn@gmail.com>
2021-05-27 15:56:48 +0300

    constsnts added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 import galleryItems from './gallery-items.js';
 
-//ul .gallery
-const galleryListEl = document.querySelector('ul.gallery');
-
-//modal window
+const galleryListEl = document.querySelector('ul.js-gallery');
 const modalWindowEl = document.getElementsByClassName('lightbox');
-
-//создаем разметку шаблонной строкой
-// // <a class="gallery__link" href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg">
+const modalWindowCloseBtn = document.querySelector(
+  '[data-action = close-lightbox]',
+);
+const modalLightBoxOverlayEl = document.querySelector('.lightbox__overlay');
+const modalLightBoxImageEl = document.querySelector('.lightbox__image');
 
 function createPicturesMarkup(pics) {
   return pics
@@ -32,11 +31,3 @@ function addPictureElementsToList(item) {
   galleryListEl.insertAdjacentHTML('beforeend', createPicturesMarkup(item));
 }
 addPictureElementsToList(galleryItems);
-
-// li
-const itemEl = document.querySelectorAll('.gallery__item');
-
-function openModalWindow(el) {
-  return el.forEach(element => {});
-}
-console.log(openModalWindow(itemEl));
